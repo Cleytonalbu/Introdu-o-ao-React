2d7a2ed7cb25af99c692b7f15ffcba318a9061ed
Cleyton Albuquerque <49384769+Cleytonalbu@users.noreply.github.com>
2022-05-18 23:26:10 -0300

    Delete src/componentes directory

--- src/componentes/botao/botao.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-import './botao.css';
-
-export default function Botao(props) {
-    return ( 
-        <button onClick={()=> alert('dados enviados')} >{props.texto}</button> 
-    );   
-}
